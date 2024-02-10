9c8d5d4d3e0a79bdeb9b67d9d39366c62b2e7f23
SkylightCrash <142622411+SkylightCrash@users.noreply.github.com>
2024-02-10 13:50:49 +0100

    Update and rename header.html to common.js

--- common.js
+++ common.js
@@ -1,4 +1,5 @@
-<!-- header.html -->
+// common.js
+document.write(`
 <header>
   <img src="https://upload.wikimedia.org/wikipedia/commons/5/5d/Institution_Mongazon_-_Angers_-_20110119.jpg" alt="Bannière du Lycée" width="100%">
   <a href="index.html"><img src="https://www.mongazon.org/themes/custom/mongazon/img/logo-mongazon.png" alt="Logo Mongazon" id="logo"></a>
@@ -10,3 +11,4 @@
     <li><a href="messages.html">Messagerie</a></li>
   </ul>
 </nav>
+`);
